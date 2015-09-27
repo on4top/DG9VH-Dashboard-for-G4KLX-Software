be6eaff80e9d320634c90d6fd481a491edcdd87f
on4top <tom@on4top.be>
2015-09-27 05:19:17 +0200

    Location of binarys on DL5DI debian packages

--- remotewrapper.php
+++ remotewrapper.php
@@ -11,8 +11,13 @@ if (isset($_POST['RECONNECT']))
 if ($passwd == REMOTECONTROLPASSWD) {
 	if (isset($target)) {
 		$output = shell_exec('sudo -u pi /usr/local/bin/remotecontrold "'.$repeater.'" link "'.$reconnect.'" "'.$target.'"');
+		// Debian DL5DI binarys are in usr/bin !!
+		//  $output = shell_exec('sudo -u pi /usr/bin/remotecontrold "'.$repeater.'" link "'.$reconnect.'" "'.$target.'"');
 	} else {
 		$output = shell_exec('sudo -u pi /usr/local/bin/remotecontrold "'.$repeater.'" link "'.$reconnect.'" unlink');
+		// Debian DL5DI binarys are in usr/bin !!
+		// $output = shell_exec('sudo -u pi /usr/bin/remotecontrold "'.$repeater.'" link "'.$reconnect.'" unlink');
+		
 	}
 	echo $output;
 } else {
